5c32a1c69ff44f834418070da4ab3f609c1fe4d4
John <An3439201@gmail.com>
2025-10-15 20:22:37 +0700

    feat: implement forget password feature with UI and component structure

--- eau-claire-farm-management/src/app/app.ts
+++ eau-claire-farm-management/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 import { Login } from './auth/login/login';
+import { ForgetPassword } from './auth/forget-password/forget-password';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SharedModule, Login],
+  imports: [RouterOutlet, SharedModule, Login, ForgetPassword],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- eau-claire-farm-management/src/app/auth/forget-password/forget-password.spec.ts
+++ eau-claire-farm-management/src/app/auth/forget-password/forget-password.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Register } from './register';
+import { ForgetPassword } from './forget-password';
 
-describe('Register', () => {
-  let component: Register;
-  let fixture: ComponentFixture<Register>;
+describe('ForgetPassword', () => {
+  let component: ForgetPassword;
+  let fixture: ComponentFixture<ForgetPassword>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Register]
+      imports: [ForgetPassword]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Register);
+    fixture = TestBed.createComponent(ForgetPassword);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
